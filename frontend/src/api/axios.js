9cf3470be299373ff46c6d15216b6d7c6bf7e8a5
Aarush2701 <aarushg501@gmail.com>
2025-08-15 13:43:50 +0530

    first commitv9

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'hhttps://splitwise-appbackend.onrender.com',
+  baseURL: 'https://splitwise-appbackend.onrender.com',
   
 });
 
